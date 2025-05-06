0ff03a8e0642cfb04aa27fa71d71cccfe5a07d2d
hsiang2 <q224399377@gmail.com>
2025-05-07 00:05:28 +0100

    first commit

---
+++ src/data/generated/bib.js
@@ -0,0 +1,105 @@
+﻿define({ entries : {
+    "bikou2024recruitment": {
+        "author": "Bikou, Alexia Georgia and Deligianni, Elena and Dermiki-Gkana, Foteini and Liappas, Nikolaos and Teri\u00fas-Padr\u00f3n, Jos\u00e9 Gabriel and Jaunsar\u00e1s, Maria Eugenia Beltr\u00e1n and Cabrera-Umpi\u00e9rrez, Maria Fernanda and Kontogiorgis, Christos",
+        "doi": "10.2196/60504",
+        "journal": "Journal of Medical Internet Research",
+        "pages": "e60504",
+        "title": "Improving Participant Recruitment in Clinical Trials: Comparative Analysis of Innovative Digital Platforms",
+        "type": "article",
+        "volume": "26",
+        "year": "2024"
+    },
+    "burger2019osf": {
+        "author": "Burger, F. and Brinkman, W. P.",
+        "booktitle": "Workshop on Methodology and/or Evaluation of IVAs, July 2019, Paris, France",
+        "note": "Workshop paper",
+        "title": "Suitability of the OSF for Preregistering HCI Research: Conclusions from Case Reports",
+        "type": "inproceedings",
+        "url": "https://iva2019methodologyworkshop.wordpress.com/wp-content/uploads/2019/06/burger_iva2019_methodology_workshop_final_casereportosf.pdf",
+        "year": "2019"
+    },
+    "foster2017osf": {
+        "author": "Foster, E. D. and Deardorff, A.",
+        "doi": "10.5195/jmla.2017.88",
+        "journal": "Journal of the Medical Library Association",
+        "number": "2",
+        "pages": "203",
+        "title": "Open Science Framework (OSF)",
+        "type": "article",
+        "volume": "105",
+        "year": "2017"
+    },
+    "idowu2024mltools": {
+        "author": "Idowu, Samuel and Osman, Osman and Str\u00fcber, Daniel and Berger, Thorsten",
+        "doi": "10.1007/s10664-024-10444-w",
+        "journal": "Empirical Software Engineering",
+        "pages": "Article number 74",
+        "title": "Machine learning experiment management tools: a mixed-methods empirical study",
+        "type": "article",
+        "volume": "29",
+        "year": "2024"
+    },
+    "kwon2020platform": {
+        "author": "Kwon, Ha-Kyung and Gopal, Chirranjeevi Balaji and Kirschner, Jared and Caicedo, Santiago and Storey, Brian D.",
+        "howpublished": "\\url{https://arxiv.org/abs/2007.14443",
+        "note": "Preprint",
+        "title": "A user-centered approach to designing an experimental laboratory data platform",
+        "type": "misc",
+        "year": "2020"
+    },
+    "lunn2019pride": {
+        "author": "Lunn, Mitchell R. and Lubensky, Micah and Hunt, Carolyn and Flentje, Annesa and Capriotti, Matthew R. and Sooksaman, Chollada and Harnett, Todd and Currie, Del and Neal, Chris and Obedin-Maliver, Juno",
+        "doi": "10.1093/jamia/ocz082",
+        "journal": "Journal of the American Medical Informatics Association",
+        "number": "8-9",
+        "pages": "737\u2013748",
+        "title": "A digital health research platform for community engagement, recruitment, and retention of sexual and gender minority adults in a national longitudinal cohort study \u2013 The PRIDE Study",
+        "type": "article",
+        "volume": "26",
+        "year": "2019"
+    },
+    "penuel2007codesign": {
+        "author": "Penuel, W. R. and Roschelle, J. and Shechtman, N.",
+        "doi": "10.1142/S1793206807000300",
+        "journal": "Research and Practice in Technology Enhanced Learning",
+        "number": "1",
+        "pages": "51\u201374",
+        "title": "Designing formative assessment software with teachers: An analysis of the co-design process",
+        "type": "article",
+        "volume": "2",
+        "year": "2007"
+    },
+    "steen2013codesign": {
+        "author": "Steen, M.",
+        "doi": "10.1162/DESI_a_00207",
+        "journal": "Design Issues",
+        "number": "2",
+        "pages": "16\u201328",
+        "title": "Co-design as a process of joint inquiry and imagination",
+        "type": "article",
+        "volume": "29",
+        "year": "2013"
+    },
+    "walden2020ucd": {
+        "author": "Walden, Anita and Garvin, Lynsi and Smerek, Michelle and Johnson, Constance",
+        "doi": "10.1177/1740774520946314",
+        "journal": "Clinical Trials",
+        "number": "6",
+        "pages": "703\u2013711",
+        "title": "User-centered design principles in the development of clinical research tools",
+        "type": "article",
+        "volume": "17",
+        "year": "2020"
+    },
+    "weissgerber2019mapping": {
+        "author": "Wei\u00dfgerber, T. and Granitzer, M.",
+        "doi": "10.1515/itit-2018-0022",
+        "journal": "it \u2013 Information Technology",
+        "number": "4",
+        "pages": "197\u2013208",
+        "title": "Mapping platforms into a new open science model for machine learning",
+        "type": "article",
+        "volume": "61",
+        "year": "2019"
+    }
+}});
